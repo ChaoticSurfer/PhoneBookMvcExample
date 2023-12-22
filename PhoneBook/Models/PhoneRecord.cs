1aedbde8883512df7863b45cbb2ded844df0f8ca
Anri Kezeroti <AKezeroti@abacusnext.com>
2023-12-22 14:25:23 +0400

    Add project files.

---
+++ PhoneBook/Models/PhoneRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.Models
+{
+    public class PhoneRecord
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
